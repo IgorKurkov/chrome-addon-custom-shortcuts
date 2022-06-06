e14be39d2d8d52fb9e985e68b53c79c836162209
IgorKurkov <kurkov87@gmail.com>
2022-06-06 10:48:51 +0200

    add clickup hotkeys

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,17 @@
 chrome.commands.onCommand.addListener(function (command) {
-    const direction = command.split('-')[1];
+  debugger;
+  if (["done-task-btn", "fullscreen-btn"].includes(command)) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        done: command === "done-task-btn",
+        fullscreen: command === "fullscreen-btn",
+      });
+    });
+  } else {
+    const direction = command.split("-")[1];
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { direction });
+      chrome.tabs.sendMessage(tabs[0].id, { direction });
     });
+  }
 });
--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,13 +1,30 @@
-const container = document.getElementsByClassName('rbGOge SeRypc');
+chrome.runtime.onMessage.addListener(function (message) {
+  // GOOGLE CAL
+  const calendarContainer = document.querySelector(".rbGOge.SeRypc");
+  const clickupContainer = document.querySelector(".task__toolbar");
+  //   debugger;
 
-if (container) {
-    const containerChildren = container[0].childNodes;
+  if (calendarContainer) {
+    const containerChildren = calendarContainer.childNodes;
 
     const leftArrow = containerChildren[1];
     const rightArrow = containerChildren[2];
 
-    chrome.runtime.onMessage.addListener(function (message) {
-        const { direction } = message;
-        direction === 'left' ? leftArrow.click() : rightArrow.click();
-    });
-}
+    const { direction } = message;
+    direction === "left" ? leftArrow.click() : rightArrow.click();
+  }
+
+  // CLICKUP
+
+  if (clickupContainer) {
+    const doneButton = document.querySelector(".done-task-btn");
+    const fullscreenBtn = document.querySelector(".cu-editor-fullscreen");
+    const { done, fullscreen } = message;
+    if (done) {
+      doneButton.click();
+    }
+    if (fullscreen) {
+      fullscreenBtn.click();
+    }
+  }
+});
