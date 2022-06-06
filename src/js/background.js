3f7157baa1f14865c1efe754b27fda5b9e7fcb18
IgorKurkov <kurkov87@gmail.com>
2022-06-06 13:44:40 +0200

    delete google calendar hotkeys

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,11 @@
 chrome.commands.onCommand.addListener(function (command) {
   debugger;
-  if (["done-task-btn", "fullscreen-btn"].includes(command)) {
+  if (["done-task-btn", "fullscreen-btn", "seemore-btn"].includes(command)) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
         done: command === "done-task-btn",
         fullscreen: command === "fullscreen-btn",
+        seemore: command === "seemore-btn",
       });
     });
   } else {
--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,30 +1,19 @@
 chrome.runtime.onMessage.addListener(function (message) {
-  // GOOGLE CAL
-  const calendarContainer = document.querySelector(".rbGOge.SeRypc");
   const clickupContainer = document.querySelector(".task__toolbar");
-  //   debugger;
-
-  if (calendarContainer) {
-    const containerChildren = calendarContainer.childNodes;
-
-    const leftArrow = containerChildren[1];
-    const rightArrow = containerChildren[2];
-
-    const { direction } = message;
-    direction === "left" ? leftArrow.click() : rightArrow.click();
-  }
-
-  // CLICKUP
 
   if (clickupContainer) {
     const doneButton = document.querySelector(".done-task-btn");
+    const seemoreBtn = document.querySelector(".task-description__show-more-btn");
     const fullscreenBtn = document.querySelector(".cu-editor-fullscreen");
-    const { done, fullscreen } = message;
+    const { done, fullscreen, seemore } = message;
     if (done) {
       doneButton.click();
     }
     if (fullscreen) {
       fullscreenBtn.click();
+    }
+    if (seemore) {
+      seemoreBtn.click();
     }
   }
 });
